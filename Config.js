b56addd4b6d1b58a09533f27e804fcec9b7ccef6
festiwise <wasiedu1234@gmail.com>
2025-05-10 08:20:03 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 // Contact details
-global.ownernomer = process.env.OWNER_NOMER || "2348109263390";
+global.ownernomer = process.env.OWNER_NOMER || "233550928795";
 global.ytname = process.env.YT_NAME || "YT: @EliteProTech";
 global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
 global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '2348109263390';
-global.ownername = process.env.OWNER_NAME || 'ElitePro';
+global.ownernumber = process.env.OWNER_NUMBER || '233550928795';
+global.ownername = process.env.OWNER_NAME || 'SteezPro';
 global.botname = process.env.BOT_NAME || 'ELITE-PRO-V1';
 
 // Default settings 
@@ -19,12 +19,12 @@ global.prefa = process.env.PREFIX ? process.env.PREFIX.split(',') : ['', '!', '.
 global.autoRecording = process.env.AUTO_RECORDING === 'true';
 global.autoTyping = process.env.AUTO_TYPING === 'true';
 global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'true';
-global.autoread = process.env.AUTO_READ === 'true';
+global.autoread = process.env.AUTO_READ === 'false';
 global.autobio = process.env.AUTO_BIO !== 'false'; // default true
 global.anti92 = process.env.ANTI_92 === 'true';
 global.autoswview = process.env.AUTO_SW_VIEW !== 'false'; // default true
 global.welcome = process.env.WELCOME !== 'false'; // default true
-global.autoreact = process.env.AUTO_REACT === 'true';
+global.autoreact = process.env.AUTO_REACT === 'false';
 
 // Thumbnail profile picture
 global.elitepropp = process.env.ELITE_PRO_PP || 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg';
@@ -51,7 +51,7 @@ global.mess = {
     prem: '*⦿ This command is made for premium users.⁉️*',
     admin: '*⦿ This command is made for group admins.‼️*',
     botAdmin: '*⦿  Make bot admin to access commands⿻*',
-    owner: '*⦿This commands is made for bot owner.*',
+    owner: '*⦿This commands is made for only me.*',
     group: '*⦿ This command is made for group chat❕*',
     private: '*⦿ This command is made for private chat ⭕*',
     wait: '*_⚙️PROCESSING DATA......_*',
